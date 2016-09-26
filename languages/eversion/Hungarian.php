349d4062c9c1fc2498d91f3eb1d3b5918e5df6fd
Gábor <yesszus@gmail.com>
2016-09-26 10:02:17 +0200

    Add files via upload

---
+++ languages/eversion/Hungarian.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+define("EVERSION_U1", "A plugin.php nem található a plugin mappában");
+define("EVERSION_U2", "Az e_update.php nem található a plugin mappában");
+define("EVERSION_U3", "Frissítések ellenőrzése a kiegészítőhöz :");
+define("EVERSION_U4", "A jelenlegi verzió a szervereden :");
+define("EVERSION_U5", "Frissítések ellenőrzése");
+define("EVERSION_U6", "Nem érhető el a frissítési szerver a frissítések ellenőrizéséhez.");
+define("EVERSION_U7", "Elérhető az új verzió");
+define("EVERSION_U8", "Nos, ez béta verzió. Csak akkor használd, ha tudod mit csinálsz!");
+define("EVERSION_U9", "A kiegészítő naprakész. A legfrissebb verzióval rendelkezel.<br/><img src='".e_PLUGIN."ytm_gallery/ok.gif' border='0'>");
+define("EVERSION_U10", "A frissítési szerveren nincsenek adatok a kiegészítőről.<br/>Vedd fel a kapcsolatot a készítővel.");
+define("EVERSION_U11", "Frissítések ellenőrzése");
+define("EVERSION_U12", "Végeredmény");
+define("EVERSION_U13", "Nézd meg a részletes ismertetést, vagy töltsd le most: ");
+define("EVERSION_U14", "Klikkelj ide");
+define("EVERSION_U15", "Ismertetés :");
+define("EVERSION_U16", "Frissítés ellenőrzés");
+define("EVERSION_U17", "Kiadva :");
+define("EVERSION_U18", "Kiegészítő készítője :");
+define("EVERSION_U19", "A Plugin Manager jelentése a verzióról ");
+define("EVERSION_U20", "telepítve. Nézd meg a Plugin Managert.");
+define("EVERSION_U21", "Végleges");
+define("EVERSION_U22", "A telepített verzió régebbi, mint jelenlegi friss verzió.");
+define("EVERSION_U23","Elérhető verzió:  ");
+
+?>
